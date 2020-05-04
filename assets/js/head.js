effbfb90ffcdeaacab359e6d3ef875240579aa24
nyy28477590 <nyy28477590@gmail.com>
2020-05-04 14:31:22 +0800

    second commit

--- assets/js/head.js
+++ assets/js/head.js
@@ -1,12 +1,18 @@
 var bodyClass = document.body.classList,
-    lastScrollY = 0;
+  lastScrollY = 0;
 window.addEventListener('scroll', function(){
   var st = this.scrollY;
   // 判斷是向上捲動，而且捲軸超過 200px
-  if( st < lastScrollY) {
+  /*if( st < lastScrollY) {
     bodyClass.remove('hideUp');
   }else{
     bodyClass.add('hideUp');
+  } */
+  if(st > lastScrollY & st > 80) {
+    bodyClass.add('hideUp');
+  }else {
+    bodyClass.remove('hideUp');
   }
   lastScrollY = st;
 });
+
